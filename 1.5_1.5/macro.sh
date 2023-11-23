40a01ce3cc623e9e13293f48361748fc83e3c8ba
Matheus <matheus.btissot@gmail.com>
2023-11-23 00:19:41 -0300

    Adicionando coisas da reuniao dia 23/11

---
+++ 1.5_1.5/macro.sh
@@ -0,0 +1,161 @@
+#! /usr/bin/bash
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 13 execução"
+echo "$dt - Começando 13 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1300.wt
+cp IL.wt IL1300.wt
+cp IS.wt IS1300.wt
+cp IT.wt IT1300.wt
+cp DDzzduz.wt DDzzduz1300.wt
+cp Out.wt Out1300.wt
+cp Ratios.wt Ratios1300.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1300.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 14 execução"
+echo "$dt - Começando 14 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1400.wt
+cp IL.wt IL1400.wt
+cp IS.wt IS1400.wt
+cp IT.wt IT1400.wt
+cp DDzzduz.wt DDzzduz1400.wt
+cp Out.wt Out1400.wt
+cp Ratios.wt Ratios1400.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1400.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 15 execução"
+echo "$dt - Começando 15 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1500.wt
+cp IL.wt IL1500.wt
+cp IS.wt IS1500.wt
+cp IT.wt IT1500.wt
+cp DDzzduz.wt DDzzduz1500.wt
+cp Out.wt Out1500.wt
+cp Ratios.wt Ratios1500.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1500.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 16 execução"
+echo "$dt - Começando 16 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1600.wt
+cp IL.wt IL1600.wt
+cp IS.wt IS1600.wt
+cp IT.wt IT1600.wt
+cp DDzzduz.wt DDzzduz1600.wt
+cp Out.wt Out1600.wt
+cp Ratios.wt Ratios1600.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1600.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 17 execução"
+echo "$dt - Começando 17 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1700.wt
+cp IL.wt IL1700.wt
+cp IS.wt IS1700.wt
+cp IT.wt IT1700.wt
+cp DDzzduz.wt DDzzduz1700.wt
+cp Out.wt Out1700.wt
+cp Ratios.wt Ratios1700.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1700.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 18 execução"
+echo "$dt - Começando 18 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1800.wt
+cp IL.wt IL1800.wt
+cp IS.wt IS1800.wt
+cp IT.wt IT1800.wt
+cp DDzzduz.wt DDzzduz1800.wt
+cp Out.wt Out1800.wt
+cp Ratios.wt Ratios1800.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1800.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 19 execução"
+echo "$dt - Começando 19 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe1900.wt
+cp IL.wt IL1900.wt
+cp IS.wt IS1900.wt
+cp IT.wt IT1900.wt
+cp DDzzduz.wt DDzzduz1900.wt
+cp Out.wt Out1900.wt
+cp Ratios.wt Ratios1900.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe1900.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
+
+#---------------------------------------------
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt - Começando 20 execução"
+echo "$dt - Começando 20 execução" >> Tempo.txt
+./a.out
+
+cp Fe.wt Fe2000.wt
+cp IL.wt IL2000.wt
+cp IS.wt IS2000.wt
+cp IT.wt IT2000.wt
+cp DDzzduz.wt DDzzduz2000.wt
+cp Out.wt Out2000.wt
+cp Ratios.wt Ratios2000.wt
+mv Out.wt Ini.wt
+
+if grep -q "NaN" Fe2000.wt;
+then echo 'Achei um NaN na função distribuição e terminei a execução.' && return
+fi
